3fed31228a64de8ff1919d7183d65322c4992607
robertpiazza <piazza_robert@bah.com>
2019-11-05 11:22:47 -0800

    Added functions

--- app/static/client.js
+++ app/static/client.js
@@ -14,30 +14,6 @@ function showPicked(input) {
   reader.readAsDataURL(input.files[0]);
 }
 
-function submitHull(input) {
-  var hull_upload = el("hull-text").value;
-  if (hull_upload.length === 0) alert("Please provide a hull_number!");
-
-  el("submit-hull-button").innerHTML = "Submitting...";
-  var xhr = new XMLHttpRequest();
-  var loc = window.location;
-  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/hull_lookup`,
-    true);
-  xhr.onerror = function() {
-    alert(xhr.responseText);
-  };
-  xhr.onload = function(e) {
-    if (this.readyState === 4) {
-      var response = JSON.parse(e.target.responseText);
-      el("result_hull-label").innerHTML = `Result = ${response["hull_information"]}`;
-    }
-    el("submit-hull-button").innerHTML = "Submit";
-  };
-  var hullData = new FormData();
-  hullData.append("hull_text", hull_upload);
-  xhr.send(hullData);
-}
-
 function analyze() {
   var uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
@@ -57,6 +33,31 @@ function analyze() {
       el("result_micro-label").innerHTML = `Specific Class guess: ${response["result_micro"]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
+  };
+
+  var fileData = new FormData();
+  fileData.append("file", uploadFiles[0]);
+  xhr.send(fileData);
+}
+
+function submitHull() {
+  var hullText = el("hull-text").value;
+  if (uploadFiles.length === 0) alert("Please input a number!");
+
+  el("submit-hull-button").innerHTML = "Submitting...";
+  var xhr = new XMLHttpRequest();
+  var loc = window.location;
+  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/hull_lookup`,
+    true);
+  xhr.onerror = function() {
+    alert(xhr.responseText);
+  };
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
+      el("result_hull-label").innerHTML = `Hull number lookup: ${response["hull_information"]}`;
+    }
+    el("submit-hull-button").innerHTML = "Submit";
   };
 
   var fileData = new FormData();
