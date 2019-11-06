f84d0bd02f4453919bdabaa420f92ce9c3baedfc
robertpiazza <piazza_robert@bah.com>
2019-11-06 13:57:02 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -17,9 +17,6 @@ export_file_url_micro = 'https://drive.google.com/uc?export=download&id=1cV2PaYK
 export_file_name_micro = 'micro_export.pkl'
 
 
-#classes = ['amphib','carrier','corvette','destroyer','frigate','gunboat','mine','missile','subchaser','tender']
-classes_micro = ['arleigh_burke_destroyer', 'dayun_904_tender', 'fuchi_903_tender', 'fuqing_905_tender', 'fusu_908_tender', 'haiqing_037IS_subchaser', 'houbei_022_missile', 'houjian_houxin_037_missile', 'jiangdao_056_corvette', 'jianghu_053H1_frigate', 'jiangkai_II_054A_frigate', 'jiangkai_I_054_frigate', 'jiangwei_II_053H3_frigate', 'liaoning_001_carrier', 'luda_051_destroyer', 'luhai_051B_destroyer', 'luhu_052_destroyer', 'luyang_III_052D_destroyer', 'luyang_II_052C_destroyer', 'luyang_I_052B_destroyer', 'luzhou_051C_destroyer', 'renhai_055_destroyer', 'shanghai_III_062I_gunboat', 'sovremenny_956_destroyer', 'wasao_082_mine', 'wazang_082II_mine', 'wochi_081_mine', 'yubei_074A_amphib', 'yudeng_073III_amphib', 'yuhai_074_amphib', 'yukan_072_amphib', 'yunshu_073A_amphib', 'yuting_072II_amphib', 'yuting_III_072A_amphib', 'yuting_II_072III_amphib', 'yuzhao_071_amphib']
-
 path = Path(__file__).parent
 
 app = Starlette()
@@ -81,29 +78,35 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
+    #load hull number from webpage form
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
+    
+    #make prediction
     macro_prediction = learn_macro.predict(img)
+    
+    #prediction string
     prediction = f"{str(macro_prediction[0]).title()} ({round(macro_prediction[2].max().item()*100)}% Probability)"
-    #micro_prediction = str(learn_micro.predict(img)[0]).replace('_', ' ')
+    
     #make predictions for individual classes
     micro_prediction = learn_micro.predict(img)
     result_micro_text = str(micro_prediction[0]).replace('_',' ').title().replace("Iii", "III").replace("Ii",'II')
     result_micro = f"{result_micro_text} ({round(micro_prediction[2].max().item()*100)}% Probability)"
+    
     #combine labels and probabilities
     micro_predictions_df = pd.DataFrame({"Probability":micro_prediction[2].tolist(), "Classes":learn_micro.data.classes})
+    
     #find top 5
     top_5 = micro_predictions_df.sort_values(by="Probability", ascending = False).head()
+    
     #create combined labels and probabilites
-    top_5['Class and Probability'] = (top_5.Classes.str.replace('_',' ')+' ('+((top_5.Probability*100).round()).map(int).map(str)+'%)').apply(lambda x: x.title().replace("Iii", "III").replace("Ii",'II'))
     #capitalize each word and make sure versions stay good
-    #top_5_results = ", ".join(top_5.Combined.tolist()).title().replace("Iii", "III").replace("Ii",'II')
+    top_5['Class and Probability'] = (top_5.Classes.str.replace('_',' ')+' ('+((top_5.Probability*100).round()).map(int).map(str)+'%)').apply(lambda x: x.title().replace("Iii", "III").replace("Ii",'II'))
+    
+    #create table string
     top_5_results = top_5[['Class and Probability']].to_html(index = False)
-    #micro_prediction = micro_prediction
-    #prediction = 'big_predict'
-    #micro_prediction = 'Not currently implemented.'
-    #micro_prediction = "Not currently implemented. Comments? email piazzr2@gmail.com"
+    
     return JSONResponse({'result': prediction, 'result_micro':result_micro, 'top_5':top_5_results})
 
 @app.route('/hull_lookup', methods=['POST'])
